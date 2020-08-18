f4a49295d5dc65d352cf41367fd6347c2e6379c3
Mims Wright <mims@mimswright.com>
2020-08-18 15:57:53 -0700

    Added note about unread messages

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Hey for Franz
 This is a Franz recipe for Hey.com
 
+### About Message Count
+Unfortunately, there's no good way to get a total count of unread emails (or read emails, set aside, etc.) so we show a 1 if there are ANY unread emails.
+
 ### How to create your own Franz recipes:
 * [Read the documentation](https://github.com/meetfranz/plugins)
--- webview.js
+++ webview.js
@@ -2,10 +2,10 @@ const SELECTOR_INBOX_UNREAD = '#postings article:not([data-seen=true])';
 
 module.exports = (Franz) => {
   const getMessages = () => {
-    const allMessages = document.querySelectorAll(SELECTOR_INBOX_UNREAD).length;
+    const unreadMessages = document.querySelectorAll(SELECTOR_INBOX_UNREAD).length;
 
-    // set Franz badge
-    Franz.setBadge(allMessages > 0 ? 1 : 0);
+    // set Franz badge to 1 if there are any unread messages.
+    Franz.setBadge(unreadMessages > 0 ? 1 : 0);
   };
 
   Franz.loop(getMessages);
