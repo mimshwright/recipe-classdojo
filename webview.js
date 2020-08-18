e076b4d9df943fb80eced5e7a2eb40e6ea2b84c5
Mims Wright <mims@mimswright.com>
2020-08-18 15:54:09 -0700

    Added notifications

--- webview.js
+++ webview.js
@@ -1,53 +1,15 @@
-import { ipcRenderer } from 'electron';
-import path from 'path';
-
-const getTeamIcon = function getTeamIcon(count = 0) {
-  let countTeamIconCheck = count;
-  let bgUrl = null;
-
-  const teamMenu = document.querySelector('#team-menu-trigger');
-  if (teamMenu) {
-    teamMenu.click();
-
-    const icon = document.querySelector('.c-team_icon');
-    if (icon) {
-      bgUrl = window.getComputedStyle(icon, null).getPropertyValue('background-image');
-      bgUrl = /^url\((['"]?)(.*)\1\)$/.exec(bgUrl);
-      bgUrl = bgUrl ? bgUrl[2] : '';
-    }
-
-    setTimeout(() => {
-      document.querySelector('.ReactModal__Overlay').click();
-    }, 10);
-  }
-
-  countTeamIconCheck += 1;
-
-  if (bgUrl) {
-    ipcRenderer.sendToHost('avatar', bgUrl);
-  } else if (countTeamIconCheck <= 5) {
-    setTimeout(() => {
-      getTeamIcon(countTeamIconCheck + 1);
-    }, 2000);
-  }
-};
-// c-link p-channel_sidebar__channel p-channel_sidebar__channel--unread
-const SELECTOR_CHANNELS_UNREAD = '.p-channel_sidebar__channel--unread:not(.p-channel_sidebar__channel--muted)';
+const SELECTOR_INBOX_UNREAD = '#postings article:not([data-seen=true])';
 
 module.exports = (Franz) => {
   const getMessages = () => {
-    const directMessages = document.querySelectorAll(`${SELECTOR_CHANNELS_UNREAD} .p-channel_sidebar__badge, .p-channel_sidebar__link--unread`).length;
-    const allMessages = document.querySelectorAll(SELECTOR_CHANNELS_UNREAD).length - directMessages;
+    const allMessages = document.querySelectorAll(SELECTOR_INBOX_UNREAD).length;
 
     // set Franz badge
-    Franz.setBadge(directMessages, allMessages);
+    Franz.setBadge(allMessages > 0 ? 1 : 0);
   };
+
   Franz.loop(getMessages);
 
-  setTimeout(() => {
-    getTeamIcon();
-  }, 4000);
-
   // inject franz.css stylesheet
-  Franz.injectCSS(path.join(__dirname, 'service.css'));
+  // Franz.injectCSS(path.join(__dirname, 'service.css'));
 };
